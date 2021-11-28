4ae32a371221e3e4793a2cf45365c7a3f9f72288
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2021-11-28 20:13:57 +0200

    JS-Basics-03 Step_3 done

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,13 @@
+var text=document.getElementById("text");
+
+function fct(x){
+    if (x === 1){
+        text.style.color = "green";
+    }
+    else if (x === 2){
+        text.style.color = "red";
+    }
+    else if (x === 3){
+        text.style.color = "blue";
+    }
+}
