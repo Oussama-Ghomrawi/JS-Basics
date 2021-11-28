6451de94d11f5569c5102ebdda6c0dd96e576863
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2021-11-28 11:21:12 +0200

    Step_5 done

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function exercise(){
+var x1=document.getElementById('first_number').value;
+var x2=document.getElementById('second_number').value;
+var x3= x1 * x2;
+alert("The result of " + x1 +" * " + x2 +" is "+ x3);
+}
