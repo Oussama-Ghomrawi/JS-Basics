12207e9a1db24d7ccf66891a093e0fe982c16e8f
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2021-11-28 11:32:08 +0200

    Step_7 done

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,6 @@
+function exercise(){
+    var x1=document.getElementById('shoe_size').value;
+    var x2=document.getElementById('year').value;
+    var x3= (x1*2+5)*50-x2+1776;
+    alert("The result is " + x3);
+    }
