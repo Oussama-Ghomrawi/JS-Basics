32503891310933b1ab1150a22238fccda700e3d4
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2021-11-28 19:16:51 +0200

    JS-Basics-03 Step_2 done

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,12 @@
+//var showx=document.getElementById('show');
+//var hidex=document.getElementById('hide');
+var textx=document.getElementById('texte');
+
+function fct(x){
+    if (x === 1){
+    textx.style.display = "block";
+    }
+    else if (x === 2){
+    textx.style.display = "none";
+    }
+ }
